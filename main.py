a06db5b46ce3e90645408aced6abf7b8954baeb8
alvaro <alvaro.rccm18@gmail.com>
2026-03-24 20:16:13 +0100

    Implementado gestor de tareas simple

---
+++ main.py
@@ -0,0 +1,44 @@
+from task_manager import TaskManager
+
+def print_menu():
+    print("\n-----------Gestor de tareas inteligente-----------")
+    print("1. Añadir tarea")
+    print("2. Ver tareas")
+    print("3. Completar tarea")
+    print("4. Eliminar tarea")
+    print("5. Salir")   
+
+def main():
+
+    task_manager = TaskManager()
+    while True:
+    
+        print_menu()
+
+        choice  = input("Selecciona una opción: ")
+
+        match choice:
+            case "1": 
+                description = input("Descripción de la tarea: ")
+                task_manager.add_task(description)
+                
+            case "2":
+                task_manager.list_tasks()  
+                
+            case "3":
+                id = int(input("ID de la tarea a completar: ")) 
+                task_manager.complete_task(id)
+                
+            case "4":            
+                id = int(input("ID de la tarea a eliminar: "))
+                task_manager.delete_task(id)
+                
+            case "5":
+                print("Saliendo del gestor de tareas. ¡Hasta luego!")
+                break
+                 
+            case _:
+                print("Opción no válida. Por favor, selecciona una opción del 1 al 5.")
+
+if __name__ == "__main__":
+    main()
--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,4 @@
-class task:
+class Task:
     def __init__(self,id,description,completed=False):
         self.id = id
         self.description = description
@@ -6,3 +6,38 @@ class task:
 
     def __str__(self):
         status="✓" if self.completed else " "
+        return f"[{status}] #{self.id}: {self.description}"
+    
+class TaskManager:
+    def __init__(self):
+        self._tasks = []
+        self._next_id = 1
+
+    def add_task(self, description):
+        new_task = Task(self._next_id, description)
+        self._tasks.append(new_task)
+        self._next_id += 1
+        print(f"Tarea añadida: {description}")
+
+    def complete_task(self, id):
+        for current_task in self._tasks:
+            if current_task.id == id:
+                current_task.completed = True
+                print(f"Tarea completada: {current_task}")
+                return
+        return print(f"No se encontró la tarea con ID: #{id}")
+
+    def delete_task(self, id):
+        for current_task in self._tasks:
+            if current_task.id == id:
+                self._tasks.remove(current_task)
+                print(f"Tarea eliminada: #{id}")   
+                return
+        print(f"No se encontró la tarea con ID: #{id}")
+
+    def list_tasks(self):
+        if not self._tasks:
+            print("No hay tareas.")
+        else:
+            for current_task in self._tasks:
+                print(current_task)
